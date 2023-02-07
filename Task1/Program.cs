8c5f37b5e2cadf1fffdb93d5b8933b5f82f8cd36
Maik <samara.maik@yandex.by>
2023-02-07 13:09:22 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+System.Console.WriteLine("Веедите числа");
+string str = Console.ReadLine();
+string [] arr = str.Split();
+long [] array = new long[arr.Length];
+for (int i = 0; i < arr.Length; i++)
+{
+    array[i] = Convert.ToInt32(arr[i]);
+}
+long count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]>0)
+    {
+        count++;
+    }
+}   
+System.Console.WriteLine($"Количетсво значений больше 0 = {count}");
